14451e5dc30227ea0171ea33c9c0a79f9ea85746
David <5992264+dav166@users.noreply.github.com>
2023-08-22 17:48:25 -0500

    feat: Handle multiple operations, decimals, backspace, keyboard support, and error handling

--- script.js
+++ script.js
@@ -1,85 +1,110 @@
-function add(a, b) {return a + b;}
-function subtract(a, b) {return a - b;}
-function multiply(a, b) {return a * b;}
-function divide(a, b) {
-    if (b === 0) {return "Error: Cannot divide by zero!"};
+function add(a, b) {
+    return a + b;
+  }
+  
+  function subtract(a, b) {
+    return a - b;
+  }
+  
+  function multiply(a, b) {
+    return a * b;
+  }
+  
+  function divide(a, b) {
+    if (b === 0) {
+      return "Error: Cannot divide by zero!";
+    }
     return a / b;
-}
-
-// Testing in the browser's console
-console.log(add(5, 3)); // 8
-console.log(subtract(10, 4)); // 6
-console.log(multiply(2, 3)); // 6
-console.log(divide(9, 3)); // 3
-console.log(divide(9, 0)); // Error: Cannot divide by zero!
-
-
-// Variables for calculator operation
-let firstNumber = null;
-let operator = null;
-let secondNumber = null;
-
-function operate(operator, a, b) {
+  }
+  
+  function operate(operator, a, b) {
     switch (operator) {
-        case '+':
-            return add(a, b);
-        case '-':
-            return subtract(a, b);
-        case '*':
-            return multiply(a, b);
-        case '-':
-            return divide(a, b);
-        default:
-            return "Error: Invalid operator!";
+      case '+':
+        return add(a, b);
+      case '-':
+        return subtract(a, b);
+      case '*':
+        return multiply(a, b);
+      case '/':
+        return divide(a, b);
+      default:
+        return "Error: Invalid operator!";
     }
-}
-
-// Testing
-console.log(operate('+', 5, 3)); // 8
-console.log(operate('-, 10, 4')); // 6
-
-
-// Function to update the display
-function updateDisplay(value) {
-    document.getElementById('display').innerHTML = value;
-}
-
-// Event listeners for number buttons
-document.querySelectorAll('.number').forEach(button => {
+  }
+  
+  let firstNumber = null;
+  let operator = null;
+  let secondNumber = null;
+  
+  function updateDisplay(value) {
+    document.getElementById('display').innerText = value;
+  }
+  
+  function handleNumberInput(value) {
+    if (operator === null) {
+      firstNumber = (firstNumber || '0') + value;
+      updateDisplay(firstNumber);
+    } else {
+      secondNumber = (secondNumber || '0') + value;
+      updateDisplay(secondNumber);
+    }
+  }
+  
+  function handleBackspace() {
+    if (secondNumber !== null) {
+      secondNumber = secondNumber.slice(0, -1);
+      updateDisplay(secondNumber);
+    } else if (operator !== null) {
+      operator = null;
+    } else if (firstNumber !== null) {
+      firstNumber = firstNumber.slice(0, -1);
+      updateDisplay(firstNumber);
+    }
+  }
+  
+  document.querySelectorAll('.number').forEach(button => {
+    button.addEventListener('click', (e) => handleNumberInput(e.target.value));
+  });
+  
+  document.querySelectorAll('.operator').forEach(button => {
     button.addEventListener('click', (e) => {
-        const value = e.target.value;
-        if(firstNumber === null) {
-            firstNumber = value;
-            updateDisplay(firstNumber);
-        } else {
-            secondNumber = value;
-            updateDisplay(secondNumber);
-        }
+      operator = e.target.value;
     });
-});
-
-// Event listeners for operator buttons
-document.querySelectorAll('.operator').forEach(button => {
-    button.addEventListener('click', (e) => {
-        operator = e.target.value;
-    });
-});
-
-// Event listener for equals button
-document.getElementById('equals').addEventListener('click', () => {
+  });
+  
+  document.getElementById('equals').addEventListener('click', () => {
     if (firstNumber !== null && operator !== null && secondNumber !== null) {
-        const result = operate(operator, Number(firstNumber), Number(secondNumber));
-        updateDisplay(result);
-        firstNumber = result;
-        operator = null;
-        secondNumber= null;
+      const result = operate(operator, Number(firstNumber), Number(secondNumber));
+      updateDisplay(result);
+      firstNumber = result;
+      operator = null;
+      secondNumber = null;
     }
-});
-
-// Event listener for clear button
-document.getElementById('clear').addEventListener('click', () => {
+  });
+  
+  document.getElementById('clear').addEventListener('click', () => {
     firstNumber = null;
     operator = null;
     secondNumber = null;
     updateDisplay('0');
-});
+  });
+  
+  document.getElementById('decimal').addEventListener('click', () => {
+    if (operator === null && !firstNumber.includes('.')) {
+      handleNumberInput('.');
+    } else if (!secondNumber.includes('.')) {
+      handleNumberInput('.');
+    }
+  });
+  
+  document.getElementById('backspace').addEventListener('click', handleBackspace);
+  
+  document.addEventListener('keydown', (e) => {
+    if ('0123456789'.includes(e.key)) handleNumberInput(e.key);
+    if ('+-*/'.includes(e.key)) operator = e.key;
+    if (e.key === '.') document.getElementById('decimal').click();
+    if (e.key === 'Backspace') document.getElementById('backspace').click();
+    if (e.key === 'Enter' || e.key === '=') document.getElementById('equals').click();
+    if (e.key === 'Escape') document.getElementById('clear').click();
+  });
+  
